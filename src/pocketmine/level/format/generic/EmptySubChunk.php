830d396da18bca9c164d3ca7e4e132c56656ad9b
linuzo <linuzo@kidzzone.ca>
2016-12-24 10:11:23 -0700

    Enable strict types on SubChunk and EmptySubChunk

--- src/pocketmine/level/format/generic/EmptySubChunk.php
+++ src/pocketmine/level/format/generic/EmptySubChunk.php
@@ -18,7 +18,7 @@
  *
  *
 */
-
+declare(strict_types = 1);
 namespace pocketmine\level\format\generic;
 
 use pocketmine\utils\ChunkException;
--- src/pocketmine/level/format/generic/SubChunk.php
+++ src/pocketmine/level/format/generic/SubChunk.php
@@ -18,7 +18,7 @@
  *
  *
 */
-
+declare(strict_types = 1);
 namespace pocketmine\level\format\generic;
 
 class SubChunk{
